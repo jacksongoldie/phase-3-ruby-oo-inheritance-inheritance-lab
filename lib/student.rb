6e6e2c09a8c428abeb63486e38c4da67a6bd33a7
Goldie <goldiejackson25@yahoo.com>
2021-11-26 11:05:42 -0600

    user name test failing

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative '../lib/user.rb'
 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string_of_knowledge)
+        new_knowledge = @knowledge.append(string_of_knowledge)
+        @knowledge = new_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class User
+    attr_accessor :last_name
+
+    def initialize(first_name, last_name)
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+    def first_name=(first_name)
+        @first_name = first_name
+    end
+
+    def first_name
+        @first_name
+    end
+
+end
